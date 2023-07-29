c8af873d02757f260f1a011d73094411279f4159
Léo Peyronnet <leo.peyronnet@outlook.fr>
2023-07-29 10:00:42 +0200

    Added landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,72 @@
+import { ArrowRight } from "lucide-react";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center p-24">
-      <h1 className="font-bold text-8xl">Devyus</h1>
-      <p>Work in progress...</p>
+    <main className="dark:text-white dark:bg-black bg-img p-5 min-h-screen flex flex-col items-center justify-center">
+      <section className="text-center">
+        <h1 className="font-bold text-7xl">Devyus</h1>
+        <p>Work in progress...</p>
+      </section>
+      <section className="flex flex-wrap justify-center">
+        <a
+          href=""
+          className="grid grid-cols-[auto,1fr,auto] items-center p-4 m-4 border dark:border-slate-800 border-slate-300 rounded-lg max-w-[450px] app-el backdrop-blur-lg"
+        >
+          <Image
+            src="PeyrSharp.svg"
+            width={75}
+            height={75}
+            alt="PeyrSharp Logo"
+            className="mr-2"
+          />
+          <span>
+            <h3 className="font-bold text-lg">PeyrSharp</h3>
+            <p>A C# library designed to make developers' job easier.</p>
+          </span>
+          <ArrowRight className="icon mr-2" />
+        </a>
+        <a
+          href=""
+          className="grid grid-cols-[auto,1fr,auto] items-center p-4 m-4 border dark:border-slate-800 border-slate-300 rounded-lg max-w-[450px] app-el backdrop-blur-lg"
+        >
+          <Image
+            src="Synethia.svg"
+            width={75}
+            height={75}
+            alt="Synethia Logo"
+            className="mr-2"
+          />
+          <span>
+            <h3 className="font-bold text-lg">Synethia</h3>
+            <p>
+              A basic C# algorithm that can determine the behavior of a user
+              with an application.
+            </p>
+          </span>
+          <ArrowRight className="icon mr-2" />
+        </a>
+        <a
+          href=""
+          className="grid grid-cols-[auto,1fr,auto] items-center p-4 m-4 border dark:border-slate-800 border-slate-300 rounded-lg max-w-[450px] app-el backdrop-blur-lg"
+        >
+          <Image
+            src="XValid.svg"
+            width={75}
+            height={75}
+            alt="XValid Logo"
+            className="mr-2"
+          />
+          <span>
+            <h3 className="font-bold text-lg">XValid</h3>
+            <p>
+              A simple validator library for various things, such as email,
+              credit card, ISBN and more .
+            </p>
+          </span>
+          <ArrowRight className="icon mr-2" />
+        </a>
+      </section>
     </main>
   );
 }
